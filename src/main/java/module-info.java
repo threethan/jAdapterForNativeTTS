5920d268eac8e251c0a4fd397b555c33a4ea4a9b
threethan <ethanjmedei@gmail.com>
2024-03-20 03:52:24 -0400

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,1 +1,4 @@
-module jAdapterForNativeTTS {}
+module jAdapterForNativeTTS {
+    exports io.github.jonelo.jAdapterForNativeTTS.engines;
+    exports io.github.jonelo.jAdapterForNativeTTS.engines.exceptions;
+}
